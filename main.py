4ed5668036e0a831abf8bd5f107c35a314e8ddd5
prem <premkumar00272@gmail.com>
2026-03-30 01:02:07 +0200

    add sort endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -20,3 +20,29 @@ def anything():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def get_patient(patient_id: str = Path(..., description = "The ID of the patient", example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description = "Sort on the basis of height, weight, bmi"), order: str = Query('asc', description='sort in asc or desc order')):
+    data = load_data()
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid order select from 'asc', 'desc'")
+    
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    return sorted_data
